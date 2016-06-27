e9ad6d2fc592ac853ebb996d05c34c16b19eae1f
Vamshi Krishna N S <vamshi.krishna588@gmail.com>
2016-06-27 14:35:32 -0600

    Added Property Configuration (just in case if any .properties files added in future)

---
+++ ticket-booking-service/src/main/java/com/walmart/ticket/com/walmart/ticket/config/PropertyConfiguration.java
@@ -0,0 +1,13 @@
+package com.walmart.ticket.com.walmart.ticket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * Created by Vamshi on 6/27/2016.
+ */
+@Configuration
+@PropertySource({})
+// TODO Configure any properties files here.
+public class PropertyConfiguration {
+}
