636407d364660d1d6456ac27131374bd8ad7b4d5
Vamshi Krishna N S <vamshi.krishna588@gmail.com>
2016-06-27 13:05:45 -0600

    Initial commit

---
+++ ticket-booking-service/src/main/java/com/walmart/ticket/TicketBookingServiceApplication.java
@@ -0,0 +1,12 @@
+package com.walmart.ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketBookingServiceApplication.class, args);
+	}
+}
